7967616cb982dc4ed95a7e1106e5254c4c5aade9
Evgeny <evkoldin@mail.ru>
2020-05-06 16:54:43 +0300

    add components

---
+++ src/views/test.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    
+	
+</template>
+<script lang="ts">
+	imprt { Component, Vue } from 'vue-property-decorator';
+
+	@Component
+	export default class test extends Vue {
+	}
+</script>
+<style lang="stylus">
+
+</style>
