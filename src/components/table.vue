37993bee289e22c556f3bb7398914ffc260ca780
Evgeny <evkoldin@mail.ru>
2020-05-07 10:28:04 +0300

    Table component

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,14 +1,50 @@
 <template lang="pug">
-    
-	
+	table.table
+		thead.table__heading
+			tr.table__row
+				th.table__cell.table__cell_head(
+					v-for="headItem in headerItems"
+					:key="headItem.value"
+					v-text="headItem.text"
+				)
+		tbody
+			tr.table__row(
+				v-for="(obj, index) in items"
+				:key="`${obj}-${index}`"
+			)
+				td.table__td(
+					v-for="column in headerItems"
+				)
+					slot(
+						v-if="column.slot"
+						:name="`column-${column.value}`"
+						:value="obj[column.value]"
+					)
+					span(
+						v-else
+						v-text="obj[column.value]"
+					)
 </template>
 <script lang="ts">
-	imprt { Component, Vue } from 'vue-property-decorator';
+	import { Component, Vue, Prop } from 'vue-property-decorator';
+
+	interface IHeaderItem {
+	  text: string;
+	  value: string;
+  }
+
+  interface IItems {
+	  [key: string]: string | number;
+  }
 
 	@Component
-	export default class table extends Vue {
+	export default class Table extends Vue {
+	  @Prop({ type: Array, required: true }) headerItems!: IHeaderItem[];
+	  @Prop({ type: Array, required: true }) items!: IItems[];
 	}
 </script>
 <style lang="stylus">
-
+	.table
+		&__td
+			text-align left
 </style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,14 +1,110 @@
 <template lang="pug">
-    
-	
+	.test
+		data-table(
+			:header-items="headerItems"
+			:items="items"
+		)
+			template(#column-offRoad="{ value }")
+				input(type="radio" :checked="value" disabled)
+			template(#column-color="{ value }")
+				span.color-block(:style="{ 'background-color': value }")
+			template(#column-popularity="{ value }")
+				span.popularity__wrap
+					span.color-block.green(:style="{ width: `${value}%` }") {{ value }} %
 </template>
 <script lang="ts">
-	imprt { Component, Vue } from 'vue-property-decorator';
+	import { Component, Vue } from 'vue-property-decorator';
+	import DataTable from '@/components/table.vue'
 
-	@Component
-	export default class test extends Vue {
+	@Component({
+			components: { DataTable },
+	})
+	export default class Test extends Vue {
+	  headerItems = [
+			{
+				text: 'Марка автомобиля',
+				value: 'name',
+			},
+			{
+				text: 'Объем двигателя',
+				value: 'volume',
+			},
+			{
+				text: 'Максимальная скорость',
+				value: 'maxSpeed',
+			},
+			{
+				text: 'Внедорожник',
+				value: 'offRoad',
+		  	slot: true,
+			},
+			{
+				text: 'Цвет кузова',
+				value: 'color',
+		  	slot: true,
+			},
+			{
+				text: 'Цена $',
+				value: 'prise',
+			},
+			{
+				text: 'Популярность (1-100%)',
+				value: 'popularity',
+		  	slot: true,
+			},
+		];
+
+	  items = [
+			{
+		  	name: 'BMW m3',
+		  	volume: 2.5,
+		  	maxSpeed: 230,
+		  	color: 'black',
+		  	prise: 100000,
+		  	popularity: '70',
+				offRoad: false,
+			},
+			{
+				name: 'Honda civic',
+				volume: 1.6,
+				maxSpeed: 180,
+				color: 'yellow',
+				prise: 40000,
+				popularity: '40',
+		  	offRoad: false,
+			},
+			{
+				name: 'ZAZ 968',
+				volume: 0.8,
+				maxSpeed: 110,
+				color: 'orange',
+				prise: 1000,
+				popularity: '100',
+		  	offRoad: true,
+			},
+			{
+				name: 'Lamborghini Aventador',
+				volume: 6.5,
+				maxSpeed: 350,
+				color: 'red',
+				prise: 1500000,
+				popularity: '30',
+		  	offRoad: false,
+			}
+		]
 	}
 </script>
 <style lang="stylus">
+	.color-block
+		display inline-block
+		width 20px
+		height 20px
+		color black
+		&.green
+			background-color green
 
+	.popularity
+		&__wrap
+			display block
+			height 20px
 </style>
